2347e4540e465d2f4b6e052a7af875bc8f698b7a
Ataraba <Ataraba@ATarabaTech.com>
2024-08-15 09:50:59 -0500

    Add files via upload
    This program prints a pattern to the screen then exits.

---
+++ PrintPattern.java
@@ -0,0 +1,22 @@
+package chapter_one;
+
+//#ATarabaTechSoftware#
+//Created by: Alex Taraba
+//Created on: 8/115/2024 @ 0944
+//This program will print a pattern to the screen and then exit
+
+// Begin PrintPattern class
+public class PrintPattern {
+
+	// Begin main(String[] args) method
+	public static void main(String[] args) {
+
+		// Print pattern
+		System.out.println("   J     A     V     V     A");
+		System.out.println("   J    A A     V   V     A A");
+		System.out.println("J  J   AAAAA     V V     AAAAA");
+		System.out.println(" JJ   A     A     V     A     A");
+		
+	} // End main(String[] args) method
+
+} // End PrintPattern class
